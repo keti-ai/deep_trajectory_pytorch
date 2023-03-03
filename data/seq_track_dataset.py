2bd57b8ff0361f19e606679eccfb09966e2f5eca
syh <syh20131292@gmail.com>
2023-03-03 10:34:12 +0900

    220302 syh make architecture for train
    Bbox, Track class 추가하여 데이터세트 로딩
    
    학습테스트 완

--- data/seq_track_dataset.py
+++ data/seq_track_dataset.py
@@ -1,4 +1,4 @@
-
+import os
 
 from torch.utils.data import Dataset
 from torchvision import datasets
@@ -34,3 +34,241 @@ class BaseDataSet(Dataset):
                 data_.append(self.transforms(data_in[i]))
         return torch.Tensor(torch.stack(data_)).float().cuda(),torch.Tensor(self.label[idx]).cuda().type(torch.cuda.LongTensor)
 
+
+from PIL import Image
+
+
+class Bbox():
+    def __init__(self, url, id_, cam, proj, ext_, frame):
+        self.id = int(id_)
+        self.cam = int(cam[1:])
+        self.proj = proj
+        self.ext_ = ext_
+        self.frame = int(frame)
+        self.url = url
+
+    # def __repr__(self):
+    #     return '_'.join([str(self.id),str(self.cam),self.proj,self.ext_,str(self.frame)])
+    def __repr__(self):
+        return self.url
+
+    def get_frame(self):
+        return self.frame
+
+    def get_id(self):
+        return self.id
+
+    def get_cam(self):
+        return self.cam
+
+    def get_proj(self):
+        return self.proj
+
+    def __lt__(self, other):
+        if type(other) != Bbox:
+            return self.get_frame() < other
+        else:
+            return self.get_frame() < other.get_frame()
+
+    def __le__(self, other):
+        return self.get_frame() <= other.get_frame()
+
+    def __gt__(self, other):
+        return self.get_frame() > other.get_frame()
+
+    def __ge__(self, other):
+        return self.get_frame() <= other.get_frame()
+
+    def __eq__(self, other):
+        return self.get_frame() == other.get_frame()
+
+
+tracklet_id = 0
+
+
+class Tracklet():
+    def __init__(self, id_, cam, proj, init_box, duration, length):
+        self.length = length
+        # tracklet_id +=1
+        self.tracklet_id = tracklet_id
+        self.id = int(id_)
+        self.cam = int(cam)
+        self.proj = proj
+        self.dur = duration
+        # self.ext_=ext_
+        # self.frame=int(frame)
+        # self.url=url
+        if type(init_box)==Bbox:
+            self.bbox = [init_box]
+        else:
+            self.bbox = init_box
+
+    def get_tr_id(self):
+        return self.tracklet_id
+    def __getitem__(self,idx):
+        return self.bbox[idx]
+    def __len__(self):
+        return len(self.bbox)
+
+    def add_bbox(self, bbox):
+        if bbox.get_id() != self.id or bbox.get_cam() != self.cam or len(self) >= 120:
+            return False
+        else:
+            if self.bbox[-1] < bbox and self.bbox[-1].get_frame() + self.dur > bbox:
+                self.bbox.append(bbox)
+            else:
+                return False
+        self.bbox.sort()
+        return True
+    def add_bboxes(self,bboxes):
+        self.bbox=self.bbox+bboxes
+    def get_id(self):
+        return self.id
+
+    def get_bboxes(self):
+        return self.bbox
+
+
+
+
+class TrackDataSet(Dataset):
+    def __init__(self,root_dir,track_length):
+        self.root_dir = root_dir
+        self.track_len = track_length
+
+        self.tracks=[]
+        self.transforms=transforms.Compose([transforms.ToTensor(),
+                                            transforms.Resize((224,224))])
+
+        self.load_tracks()
+        print("Load Track done")
+        #
+        # self.data = np.random.rand(10,120,256,128,3)
+        # self.label = np.random.randint(0,1000,(10,120))
+
+    def __len__(self):
+        return len(self.tracks)
+    def __getitem__(self, idx): # idx == iter
+        '''
+        idx = [ 4,6,2,1] // batch with indexes
+        self.data = np.random.rand(10,120,256,128,3)
+        self.label = np.random.randint(0,1000,(10,120))
+        track == 120*bbox
+        '''
+        in_batch=idx.__len__()
+        in_tracks=[]
+        for i in range(len(idx)):
+            in_tracks.append(self.tracks[idx[i]])
+
+        data_=[]
+        # labels = np.random.randint(0,23,(in_batch,120))
+
+        # print("idx :",idx)
+        labels = []
+        imgs = []
+        for i in range(len(idx)):
+            label_ = []
+            imgs_ = []
+            for tr in in_tracks:
+                label_=[]
+                imgs_=[]
+                for bb in tr:
+                    label_.append(bb.get_id())
+
+                    with Image.open(os.path.join(self.root_dir,str(bb))) as image:
+                        if self.transforms:
+                            image = self.transforms(image)
+                        imgs_.append(image)
+
+            labels.append(label_)
+            imgs.append(torch.stack(imgs_))
+
+        return torch.Tensor(torch.stack(imgs)).float().cuda(),\
+            torch.Tensor(labels).cuda().type(torch.cuda.LongTensor)
+
+    # ToDo tracklet 만들기
+    # method load_tracks 에서 tracklet 을 정의해고 해당 되는  bbox 링크만 정의
+    # image 로드는 __get_item__에서 실행
+    # make Track List
+    def load_tracks(self):
+        track = []
+        list_bbox = os.listdir(self.root_dir)
+        list_bbox.sort()
+
+
+        bbox_list = []
+        for li in list_bbox:
+            bbox_list.append(Bbox(li, *li.split(".")[0].split("_")))
+
+        bbox_list.sort()
+        tracklet_id = 0
+        length = 120
+        delay = 50  # duration == 프레임간 같은 트랙이라고 용인할 frame 차이
+        tracklets = [
+            Tracklet(bbox_list[0].get_id(), bbox_list[0].get_cam(), bbox_list[0].get_proj(), bbox_list[0], delay,
+                     length)]
+        print("Make Initial Tracklets ...")
+        print("Tracklet Delay Frame : ", delay)
+        for bb in bbox_list:
+            is_add = False
+            for tracklet in tracklets:
+                if tracklet.add_bbox(bb):
+                    is_add = True
+                    break
+            if is_add:
+                continue
+            tracklets.append(Tracklet(bb.get_id(), bb.get_cam(), bb.get_proj(), bb, delay, length))
+            tracklet_id += 1
+        print("Gen initial Tracklet : ", tracklet_id)
+
+        track_120=[]
+        for tr in tracklets:
+            if len(tr) == 120:
+                track_120.append(tr)
+        indee=0
+        pos_tracks = []
+        negee_tracks = []
+        neg_tracks= []
+        for tr in track_120:
+            if indee>=200:
+                in_boxes=tr.get_bboxes()
+                negee_track.append(Tracklet(in_boxes[0].get_id(), in_boxes[0].get_cam(), in_boxes[0].get_proj(), in_boxes[:60], delay, length))
+                negee_track.append(
+                    Tracklet(in_boxes[0].get_id(), in_boxes[0].get_cam(), in_boxes[0].get_proj(), in_boxes[60:], delay,
+                             length))
+            else:
+                pos_tracks.append(tr)
+        while len(negee_tracks)!=0:
+            in_tr = negee_tracks.pop(0)
+            pop_ind = 0
+            get_=False
+            for i, tr in enumerate(negee_tracks):
+                if in_tr.get_id() != tr.get_id():
+                    get_=True
+                    in_tr.add_bboxes(tr)
+                    pop_ind = i
+                    negee_tracks.pop(pop_ind)
+                    break
+            if get_:
+                neg_tracks.append(in_tr)
+
+
+
+        self.tracks=pos_tracks+neg_tracks
+        #
+        # for filename in sorted(os.listdir(self.root_dir)):
+        #     if filename.endswith('.jpg') or filename.endswith('.png'):
+        #         gt_track,cam,proj,_,frame=filename.split(".")[0].split("_")
+        #
+        #         image_path = os.path.join(self.root_dir, filename)
+        #
+        #         #
+        #
+        #         track.append(image_path)
+        #
+        #         if len(track) == self.track_len:
+        #             self.tracks.append(track)
+        #             track = []
+        #
+        # if track:
+        #     self.tracks.append(track)
--- train.py
+++ train.py
@@ -3,13 +3,44 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import numpy as np
+
 # Logging
 import visdom
 import datetime
-vis = visdom.Visdom()
-EXP="Train Cleaving\n"
 # Text
-vis.text(EXP+"<br>"+str(datetime.datetime.now()),env="main")
+class VisLogger():
+    def __init__(self,title="Test",env_="main",legends=["1","2"]):
+        self.vis = visdom.Visdom(env=env_)
+        self.vis.close(env=env_)
+        # EXP = "Train Cleaving\n"
+        # self.vis.text(EXP + "<br>" + str(datetime.datetime.now()), env="main")
+        self.data_len=0
+        self.wins=[]
+        self._x=0
+        self.title=title
+        self.legends=legends
+    def vis_log(self,win_name,x_, val):
+
+        # val=torch.view()
+        self.data_len =len(val)
+        if self.data_len > 1:
+            val = [li.item() for li in val]
+            self.vis.line(Y=[val],X=[x_],win=win_name, update="append" ,opts=dict(title=self.title, legend=self.legends,showlegend=True))
+        else:
+            self.vis.line(Y=[val], X=[x_], win=win_name, update="append")
+
+        self.wins.append(win_name)
+        self.wins=list(set(self.wins))
+        self._x=x_
+    def vis_img(self,win_name,img):
+        if len(img.shape)>3:
+            self.vis.images(img,win=win_name)
+        else:
+            self.vis.image(img,win=win_name)
+
+
+
 from model.seresnext import seresnext50_32x4d
 # from torch.nn.utils.rnn import PackedSequence
 class Cleaving(nn.Module):
@@ -25,8 +56,16 @@ class Cleaving(nn.Module):
         self.siamModel = SiameseBiGRU(track_len,feat_size, hidden_size, num_layers, dropout).cuda()
         self.feat_fcn = nn.Linear(256, id_len)
 
-        self.srh_fcn = nn.Linear(track_len - 1, track_len - 1)
-        self.pur_fcn = nn.Linear(track_len - 1, 2)
+        # self.srh_fcn = nn.Linear(track_len - 1, track_len - 1)
+        self.srh_fcn = nn.Sequential(
+            nn.Linear(track_len - 1, track_len - 1),
+            nn.Sigmoid()
+        )
+        self.pur_fcn = nn.Sequential(
+            nn.Linear(track_len - 1, 2),
+            nn.Linear(2,1),
+            nn.Sigmoid()
+        )
 
         self.width = wh[0]
         self.height = wh[1]
@@ -60,8 +99,8 @@ class Cleaving(nn.Module):
 
         srh_output = self.srh_fcn(phi_d)
         pur_output = self.pur_fcn(phi_d)
-
         return gru_output_f,gru_output_b,srh_output,pur_output
+
 
 
 
@@ -87,7 +126,7 @@ class SiameseBiGRU(nn.Module):
             output = output[:,:,self.hidden_size:]
             return output.view(len(x),-1, self.hidden_size),h_out
     def forward(self, x,x_): # x shape == L*Hin
-        print("x.shape ",x.shape)
+        # print("x.shape ",x.shape)
         # x.shape = L, batch, h_in
         out_forward=[]
         out_backward=[]
@@ -116,34 +155,10 @@ class SiameseBiGRU(nn.Module):
                  num_classes=1000):
 '''
 # Define the hyperparameters
-feat_size = 2048 # size of input features
-hidden_size = 256
-num_layers = 4
-dropout = 0.2
-learning_rate = 0.001
-
-# Tracklet info
-track_len_in = 120
-
-# Create the model
-model = Cleaving(track_len=track_len_in,id_len=1000,feat_size=feat_size, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout).cuda()
-
-# Define the loss function and optimizer
-lam_feat = 1
-lam_search = 1
-lam_pur = 1
-
-criterion_gru = torch.nn.CrossEntropyLoss()
-criterion_srh = torch.nn.CrossEntropyLoss()
-criterion_pur = torch.nn.CrossEntropyLoss()
-
-# cleav_loss = lam_feat*criterion_gru + lam_search*criterion_srh + lam_pur * criterion_pur
-
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from data.seq_track_dataset import BaseDataSet
+from data.seq_track_dataset import BaseDataSet,TrackDataSet
 from torch.utils.data.sampler import BatchSampler, SequentialSampler
 
 import time
@@ -171,56 +186,107 @@ def base_collate_fn(samples):
         label_.append(samples[i][1])
     return samples#{"input":in_,"label":label_}
 
-basedataset = BaseDataSet()
-batch_size_= 1
-dataloader = BaseDataLoader(
-    dataset=basedataset,
-    collate_fn=base_collate_fn,
-    batch_size=batch_size_,
-    sampler=BatchSampler(
-        SequentialSampler(basedataset),
-        batch_size=1,
-        drop_last=False
-    )
-)
 # Train the model
-num_epochs = 10
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    for i, data in enumerate(dataloader):
-        batch_size = data.__len__()
-        tracklets=[]
-        labels = []
-        for j in range(batch_size):
-            tracklets.append(data[j][0])
-            labels.append(data[j][1].squeeze())
-        # print("tracklet.__len__():", tracklet.__len__())
-        # Zero the parameter gradients
-        optimizer.zero_grad()
-        # print(tracklet.type)
-        # Forward + backward + optimize
-        gru_out_f,gru_out_b,srh_out,pur_out = model(torch.stack(tracklets))
 
-        label=torch.stack(labels)
+import os
+if __name__ == "__main__":
 
-        # loss = criterion(output1 - output2, label.float())
-        loss_=0
-        loss_gru=0
-        loss_srh=0
-        loss_pur=0
-        for j in range(batch_size):
-            loss_gru+=criterion_gru(gru_out_f[j],label[j])+\
-                      criterion_gru(gru_out_b[j],label[j])
-            loss_srh+=criterion_srh(srh_out[j],)
-            loss_pur+=criterion_pur(pur_out[j],len(set(label.numpy())))
+    vis_log=VisLogger(title="Test_loss",env_="main",legends=["feat_loss","srh_loss","pur_loss","loss"])
+    num_epochs = 10
 
-        loss_=lam_feat*loss_gru+lam_search*loss_srh+lam_pur*loss_pur
-        loss_.backward()
-        optimizer.step()
+    feat_size = 2048  # size of input features
+    hidden_size = 256
+    num_layers = 4
+    dropout = 0.2
+    learning_rate = 0.1
 
-        # Print statistics
-        running_loss += loss_.item()
-        print('[Epoch %d, Iteration %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
-        if i % 100 == 99:
-            print('[Epoch %d, Iteration %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
-            running_loss = 0.0
+    # Tracklet info
+    track_len_in = 120
+
+    # Create the model
+    model = Cleaving(track_len=track_len_in, id_len=22, feat_size=feat_size, hidden_size=hidden_size,
+                     num_layers=num_layers, dropout=dropout).cuda()
+
+    # Define the loss function and optimizer
+    lam_feat = 1
+    lam_search = 5
+    lam_pur = 1
+
+    criterion_gru = torch.nn.CrossEntropyLoss()
+    criterion_srh = torch.nn.CrossEntropyLoss()
+    criterion_pur = torch.nn.BCELoss()
+
+    # cleav_loss = lam_feat*criterion_gru + lam_search*criterion_srh + lam_pur * criterion_pur
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # basedataset = BaseDataSet()
+
+    basedataset = TrackDataSet(root_dir="/media/syh/ssd2/data/ReID/bounding_box_train",track_length=120)
+    batch_size_ = 1
+    dataloader = BaseDataLoader(
+        dataset=basedataset,
+        collate_fn=base_collate_fn,
+        batch_size=batch_size_,
+        sampler=BatchSampler(
+            SequentialSampler(basedataset),
+            batch_size=1,
+            drop_last=False
+        )
+    )
+
+    iter = 0
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+
+        for i, data in enumerate(dataloader):
+            iter+=1
+            batch_size = data.__len__()
+            tracklets = []
+            labels = []
+            for j in range(batch_size):
+                tracklets.append(data[j][0])
+                labels.append(data[j][1].squeeze())
+            # print("tracklet.__len__():", tracklet.__len__())
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+            # print(tracklet.type)
+            # Forward + backward + optimize
+            gru_out_f, gru_out_b, srh_out, pur_out = model(torch.stack(tracklets))
+
+            label = torch.stack(labels)
+
+            # loss = criterion(output1 - output2, label.float())
+            loss_ = 0
+            loss_gru = 0
+            loss_srh = 0
+            loss_pur = 0
+
+
+            for j in range(batch_size):
+                # ToDo search network target 설정
+                loss_gru += criterion_gru(gru_out_f[j], label[j]) + \
+                            criterion_gru(gru_out_b[j], label[j])
+                loss_srh += criterion_srh(srh_out[j], torch.Tensor([label[j][i] == label[j][i + 1] for i in range(len(label[j]) - 1)]).cuda())
+                loss_pur += criterion_pur(pur_out[j], torch.Tensor([int(len(torch.unique(label))==1)]).cuda())
+
+            loss_ = lam_feat * loss_gru + lam_search * loss_srh + lam_pur * loss_pur
+            # vis_log.vis_log(win_name="main/loss",x_=iter,val=[loss_gru,loss_srh,loss_pur,loss_])
+            loss_.backward()
+            optimizer.step()
+
+            # Print statistics
+            running_loss += loss_.item()
+
+            # print('[Epoch %d, Iteration %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
+            vis_log.vis_log(win_name="main/loss", x_=iter, val=[loss_gru, loss_srh, loss_pur, loss_])
+            vis_log.vis_log(win_name="main/loss_Gru", x_=iter, val=loss_gru)
+            vis_log.vis_log(win_name="main/loss", x_=iter, val=loss_srh)
+            vis_log.vis_log(win_name="main/loss", x_=iter, val=loss_pur)
+            if i % 100 == 99:
+                vis_log.vis_img(win_name="main/input", img=data[0][0][0][::20])
+                print('[Epoch %d, Iteration %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
+                running_loss = 0.0
+                PATH="%d_model.pth"%(iter)
+                PATH=os.path.join("/media/syh/hdd/checkpoints/deep_trajectory",PATH)
+                torch.save(model, PATH)
